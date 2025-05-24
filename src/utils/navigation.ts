ffac8a23e4c5b684a27bfc3bec2cb18f54677c76
itsabubakar <seeker99157@gmail.com>
2025-05-24 17:16:21 +0100

    Refactor code structure for improved readability and maintainability

--- src/utils/navigation.ts
+++ src/utils/navigation.ts
@@ -1,4 +1,7 @@
 export const navigationLinks = [
-  { href: "/about", label: "About" },
+  { href: "/about", label: "About " },
+  { href: "/what-we-do", label: "What We Do" },
+  { href: "/inganci-centre", label: "Inganci Centre" },
+  { href: "https://www.affest.com.ng", label: "AFFEST" },
   { href: "/contact", label: "Contact" },
 ];
