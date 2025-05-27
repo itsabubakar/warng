5e2c9436204b90908d0cdb66510dcad1799c82cf
itsabubakar <seeker99157@gmail.com>
2025-05-27 06:43:12 +0100

    Add team member images and update contact information for Women Aware of Rights Initiative

--- src/utils/navigation.ts
+++ src/utils/navigation.ts
@@ -1,6 +1,7 @@
 export const navigationLinks = [
   { href: "/about", label: "About " },
-  { href: "/what-we-do", label: "What We Do" },
+  { href: "/advocacy", label: "Advocacy" },
+  { href: "/community-service", label: "Community Service" },
   { href: "/inganci-centre", label: "Inganci Centre" },
   { href: "https://www.affest.com.ng", label: "AFFEST" },
   { href: "/contact", label: "Contact" },
